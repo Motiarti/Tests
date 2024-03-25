667d53096a9c6e34759c0ab5909858f5cdd9bcbb
nektonick <nik-1905@mail.ru>
2024-03-25 22:47:49 +0300

    Move accountList from App to Host

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,34 +2,21 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    static List<Account> accountList = Host.generateAccounts();
-
     public static void main(String[] args) {
-        int accountId = getAccountId();
-        Account account = selectAccount(accountList, accountId);
+        Host host = new Host();
+        int accountId = getAccountId(host);
+        Account account = host.selectAccount(accountId);
         AutomaticTallerMachine atm = new AutomaticTallerMachine(account);
         atm.displayMainMenu();
     }
 
-    private static int getAccountId() {
+    private static int getAccountId(Host host) {
         Scanner sc = new Scanner(System.in);
         int currentAccountId;
         do {
             System.out.print("¬ведите номер счета: ");
             currentAccountId = sc.nextInt();
-        } while(!Host.isAccountExists(accountList, currentAccountId));
+        } while(!host.isAccountExists(currentAccountId));
         return currentAccountId;
-    }
-
-    private static Account selectAccount(List<Account> accountList, int currentAccountId) {
-        for (Account account : accountList) {
-            if (currentAccountId == account.getId()) {
-                return account;
-            }
-        }
-        throw new RuntimeException(String.format("—чета с номером %d не существует", currentAccountId));
-
-        // TODO: do same thing with stream api:
-        // return accountList.stream().filter(/*YOUR CODE HERE*/).findFirst().orElseThrow(/*CREATE EXCEPTION WHICH SAYS "ACCOUNT WITH ID(currentAccountId) DOES NOT EXIST"*/);
     }
 }
--- src/main/java/Host.java
+++ src/main/java/Host.java
@@ -5,7 +5,13 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Host {
-    public static List<Account> generateAccounts() {
+    private final List<Account> accounts;
+
+    public Host() {
+        this.accounts = generateAccounts();
+    }
+
+    private static List<Account> generateAccounts() {
         double initialBalance = 10000.0;
         int numberOfAccounts = 10;
         List<Account> output = new ArrayList<>();
@@ -18,7 +24,7 @@ public class Host {
         // return IntStream.range(/*YOU CODE HERE*/).mapToObj(/*YOUR CODE HERE*/).toList();
     }
 
-    public static boolean isAccountExists(List<Account> accounts, int id) {
+    public boolean isAccountExists(int id) {
         for (Account account : accounts) {
             if (account.getId() == id) {
                 return true;
@@ -28,5 +34,19 @@ public class Host {
 
         // TODO: do same thing with stream api in one line:
         // return accounts.stream().anyMatch(/*YOUR CODE HERE*/);
+
+        // Alternative: call selectAccount(id), return true if no exception happened, and false if exception happened
+    }
+
+    public Account selectAccount(int currentAccountId) {
+        for (Account account : accounts) {
+            if (currentAccountId == account.getId()) {
+                return account;
+            }
+        }
+        throw new RuntimeException(String.format("Счета с номером %d не существует", currentAccountId));
+
+        // TODO: do same thing with stream api:
+        // return accounts.stream().filter(/*YOUR CODE HERE*/).findFirst().orElseThrow(/*CREATE EXCEPTION WHICH SAYS "ACCOUNT WITH ID(currentAccountId) DOES NOT EXIST"*/);
     }
 }
